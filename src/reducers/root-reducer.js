4ace578e2fdff237edbdc4d8eb297b88e80bf7ca
David Ryan <djrdev@icloud.com>
2018-01-04 09:40:40 -0700

    import to rootreducer

--- src/reducers/root-reducer.js
+++ src/reducers/root-reducer.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-import { fake } from './fake-reducer';
+import FetchHouseReducer from './FetchHouseReducer';
 
 const rootReducer = combineReducers({
-  fake
+  houses: FetchHouseReducer
 });
 
 
