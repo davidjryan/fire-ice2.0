cc547d82cc5c43d7fb12bc4211c2ba240991968c
David Ryan <djrdev@icloud.com>
2018-01-04 14:30:21 -0700

    reducer tests

--- src/reducers/tests/fetchHouse.test.js
+++ src/reducers/tests/fetchHouse.test.js
@@ -1,4 +1,5 @@
 import FetchHouseReducer from '../FetchHouseReducer';
+import FetchHouseData from '../../actions/FetchHouseData';
 
 describe('movies reducer', () => {
   const mockHouseData = { houses: [{ name: "Lannister" }] };
@@ -9,6 +10,35 @@ describe('movies reducer', () => {
       houseData: [],
       hasErrored: false,
       errorMessage: null,
+    });
+  });
+
+  it('should have an array of house data if type FETCHING_HOUSE_DATA_SUCCESS', () => {
+    const action = { type: 'FETCHING_HOUSE_DATA_SUCCESS', mockHouseData };
+
+    expect(FetchHouseReducer({}, action).length).toEqual(mockHouseData.length);
+  });
+
+
+  it('should return true if FETCHING_HOUSE_DATA', () => {
+    const action = { type: 'FETCHING_HOUSE_DATA' };
+
+    expect(FetchHouseReducer({}, action)).toEqual({
+      isLoading: true,
+      houseData: null,
+      hasErrored: false,
+      errorMessage: null,
+    });
+  });
+
+  it('should return true if FETCHING_HOUSE_DATA_FAIL', () => {
+    const action = { type: 'FETCHING_HOUSE_DATA_FAIL', payload: "Type 404" };
+
+    expect(FetchHouseReducer({}, action)).toEqual({
+      isLoading: false,
+      houseData: [],
+      hasErrored: true,
+      errorMessage: action.payload,
     });
   });
 });
