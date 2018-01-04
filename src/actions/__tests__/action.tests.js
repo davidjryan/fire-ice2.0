952f3421d073ef43e4e9ce6d3cd6c110d159f359
David Ryan <djrdev@icloud.com>
2018-01-04 13:34:42 -0700

    trying to test actions

---
+++ src/actions/__tests__/action.tests.js
@@ -0,0 +1,26 @@
+import * as actions from '../index';
+
+describe('fetch house actions', () => {
+  it('FetchHouseData return house data', async () => {
+    const houses = [{ name: 'snow' }, { name: 'Lannister' }, { name: 'Dorne' }];
+    const expected = {
+      type: 'FETCHING_HOUSE_DATA_SUCCESS',
+      houses
+    };
+
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      status: 200,
+      json: () => Promise.resolve(
+        {
+          data: [
+            { name: 'snow' }, { name: 'Lannister' }, { name: 'Dorne' }
+          ],
+          message: "Retrieved Houses",
+          status: "success"
+        }
+      )
+    }));
+
+    const houseSetRes = await actions.FetchHouseData();
+    expect(houseSetRes).toEqual(expected);
+});
